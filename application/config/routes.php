627237708e1c797de840ebb785345cc3c238f698
Toshihiro Murata <eb68276@jp.ibm.com>
2015-01-20 15:50:39 +0900

    Add New MVC and .htaccess

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,6 +38,9 @@
 |
 */
 
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+
 //$route['default_controller'] = "welcome";
 $route['default_controller'] = "pages/view";
 $route['(:any)'] = 'pages/view/$1';
